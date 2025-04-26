395944a0eb1af106e810740335fb2e28e446b724
bintuulugbek <163106257+bintuulugbek@users.noreply.github.com>
2025-04-26 08:18:41 +0500

    Update homework.py

--- lesson-1/homework/homework.py
+++ lesson-1/homework/homework.py
@@ -1,1 +1,11 @@
+1 Given a side of square. Find its perimeter and area.
+side = int(input('Give the side of square :')) perimeter = 4 * side area = side * side print(f'the perimeter of the square is {perimeter} and its area is {area}')
 
+2 Given diameter of circle. Find its length.
+diameter = int(input('Give the diameter of circle: ')) circleLength = 3.14 * diameter circleLength
+
+3 Given two numbers a and b. Find their mean.
+a = int(input()) b = int(input()) mean = (a + b)/2 mean
+
+4 Given two numbers a and b. Find their sum, product and square of each number.
+a = int(input()) b = int(input()) sum = a + b product = a * b square1 = pow(a, 2) square2 = pow(b, 2) print(sum, product, square1, square2)
